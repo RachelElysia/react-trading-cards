04b8ecaac12cb473efd6a266688255504e3acafd
Elizabeth Jamison <ejamison3@gmail.com>
2021-01-30 21:10:45 +0000

    Populate Trading Cards using React

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -2,8 +2,13 @@
 
 function Homepage() {
   return (
-    <div>
-      This component needs to be implemented.
+    <div className="flex-box">
+      
+      <h1>Welcome User!</h1>
+      <p><a href="/cards">Trading Cards</a></p>
+      <img src="/static/img/balloonicorn.jpg" alt="Picture of Balloonicorn"/>
+
+
     </div>
   );
 }
